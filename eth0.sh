67cb8f02c8becfef6fc2cffbbde3162d9e304fc3
ukendt79 <105305285+ukendt79@users.noreply.github.com>
2023-03-18 14:25:48 +0300

    Add files via upload

---
+++ eth0.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+sudo apt update && sudo apt install default-jre macchanger python3-pip dialog figlet -y
+cd ~/Downloads && wget https://download.i2p2.no/releases/2.1.0/i2pinstall_2.1.0.jar
+java -jar i2pinstall_2.1.0.jar #i2p'yi /home/kullanici/i2p/ adresine kur
+sudo rm -rf /etc/resolv.conf && sudo touch /etc/resolv.conf
+sudo bash -c "echo 9.9.9.9 > /etc/resolv.conf"
+sudo bash -c "echo 149.112.112.112 >> /etc/resolv.conf"
+sudo chattr +i /etc/resolv.conf
+sudo /sbin/ifconfig eth0 down
+sudo macchanger -r eth0
+sudo /sbin/ifconfig eth0 up
+sudo systemctl restart NetworkManager
+sudo pip3 install protonvpn-cli
+sudo rm -rf /var/log/*
+sudo rm -rf /var/tmp/*
+cd ~/i2p && ./i2prouter start
+rm -rf ~/Downloads/i2pinstall_2.1.0.jar
+sudo protonvpn init #bu komut ile proton hesabinizi giriyorsunuz, daha sonra "sudo protonvpn c" komutu ile sunuculara baglaniyorsunuz.
+figlet "ukendt79"
+echo "successfull"
